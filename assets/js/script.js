65e612c82afe6cbfc231bc139db617a850c88c97
justin <justinrvillena@gmail.com>
2021-04-26 20:09:16 -0700

    javascript outline for develop branch created

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// main variables
+
+// form submit handler
+
+// save search function
+
+// camp site search
+
+// weather display
+
+// image elements
+
+// span to hold previous camp site elements
+
+// span to hold weather elements
+
+// containers
+
+// create date element
+
+// past searches
