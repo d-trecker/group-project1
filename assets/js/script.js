4b5cf134f0adae435ef74c3a2a59d423d2474dda
d trec <dylan.j.trecker@gmail.com>
2021-04-29 09:49:16 -0700

    Edited JS to link camping API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,76 +1,77 @@
 // main variables
-var cityFormEl=document.querySelector("#city-search-form");
-var weatherContainerEl=document.querySelector("#current-weather-container");
+var cityFormEl = document.querySelector("#city-search-form");
+var weatherContainerEl = document.querySelector("#current-weather-container");
 var citySearchInputEl = document.querySelector("#searched-city");
 var forecastTitle = document.querySelector("#forecast");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
-
+var searchButton = document.querySelector("#search-btn");
 // form submit handler
 
-var formSumbitHandler = function(event){
-    event.preventDefault();
-    var city = cityInputEl.value.trim();
-    if(city){
-        getCityCampsite(city);
-        getCityWeather(city);
-        cities.unshift({city});
-        cityInputEl.value = "";
-    } else{
-        alert("Please enter a City");
-    }
-    saveSearch();
-    pastSearch(city);
-}
+var formSumbitHandler = function (event) {
+  event.preventDefault();
+  var city = cityInputEl.value.trim();
+  if (city) {
+    getCityCampsite(city);
+    getCityWeather(city);
+    cities.unshift({ city });
+    cityInputEl.value = "";
+  } else {
+    alert("Please enter a City");
+  }
+  saveSearch();
+  pastSearch(city);
+};
 
-var saveSearch = function(){
-    localStorage.setItem("cities", JSON.stringify(cities));
+var saveSearch = function (city) {
+  // localStorage.setItem("cities", JSON.stringify(cities));
 
-    const response = await fetch("https://cryptic-castle-96421.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=camping&location=california", {
-    method: 'GET',
-    headers: {
-        "accept": "application/json",
+  fetch(
+    "https://cryptic-castle-96421.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=camping&location=" +
+      city,
+    {
+      method: "GET",
+      headers: {
+        accept: "application/json",
         "x-requested-with": "xmlhttprequest",
-        "Access-Control-Allow-Origin":"*",
-        Authorization: `Bearer YOUR_API_KEY_HERE`,
+        "Access-Control-Allow-Origin": "*",
+        Authorization: `Bearer xRK7QJkZKuvEejlJZ7xTjEJ03Vls_YJgj5dW4Ah110WMRcrCypZe_2ONCtFeuUZT7yKB88Xb2rzMEIQO8TNJfeLt6U1i7TU3w935Eq_usonrRs1nthQCdVguDPKIYHYx`,
         "Content-type": "application/json",
-    },
-  }).then(response => response.json())
-  .then(response => console.log(response));
-
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((response) => console.log(response));
+  displayResults();
 };
 
-var get5Day = function(city){
-    var apiKey = "844421298d794574c100e3409cee0499"
-    var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`
+var get5Day = function (city) {
+  var apiKey = "844421298d794574c100e3409cee0499";
+  var apiURL = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`;
 
-    fetch(apiURL)
-    .then(function(response){
-        response.json().then(function(data){
-           display5Day(data);
-        });
+  fetch(apiURL).then(function (response) {
+    response.json().then(function (data) {
+      display5Day(data);
     });
+  });
 };
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+// $.ajax(settings).done(function (response) {
+//   console.log(response);
+// });
 
-var displayCampsite = function(_cityWeather, _cityCampsite, searchCity){
-    
-    
-    campsiteContainerEl.textContent="";
-    weatherContainerEl.textContent= "";  
-    citySearchInputEl.textContent=searchCity;
-}
+var displayCampsite = function (_cityWeather, _cityCampsite, searchCity) {
+  campsiteContainerEl.textContent = "";
+  weatherContainerEl.textContent = "";
+  citySearchInputEl.textContent = searchCity;
+};
 
- 
 // save search function
 
 // camp site search
 
 // weather display
 //Downloaded from https://www.codeseek.co/Alaa--/local-weather-app-using-flexbox-gaMOWQ */
- 
+
 // image elements
 
 // span to hold previous camp site elements
@@ -83,24 +84,37 @@ var displayCampsite = function(_cityWeather, _cityCampsite, searchCity){
 
 // past searches
 
-var pastSearch = function(pastSearch){
- 
-    // console.log(pastSearch)
+var pastSearch = function (pastSearch) {
+  // console.log(pastSearch)
 
-    pastSearchEl = document.createElement("button");
-    pastSearchEl.textContent = pastSearch;
-    pastSearchEl.classList = "d-flex w-100 btn-light border p-2";
-    pastSearchEl.setAttribute("data-city",pastSearch)
-    pastSearchEl.setAttribute("type", "submit");
+  pastSearchEl = document.createElement("button");
+  pastSearchEl.textContent = pastSearch;
+  pastSearchEl.classList = "d-flex w-100 btn-light border p-2";
+  pastSearchEl.setAttribute("data-city", pastSearch);
+  pastSearchEl.setAttribute("type", "submit");
 
-    pastSearchButtonEl.prepend(pastSearchEl);
-}
+  pastSearchButtonEl.prepend(pastSearchEl);
+};
 
+var pastSearchHandler = function (event) {
+  var city = event.target.getAttribute("data-city");
+  if (city) {
+    getCityWeather(city);
+    getCityCampsite(city);
+  }
+};
 
-var pastSearchHandler = function(event){
-    var city = event.target.getAttribute("data-city")
-    if(city){
-        getCityWeather(city);
-        getCityCampsite(city);
-    }
-}
+// function displayResults(data) {
+//   console.log("resultsDisplayed");
+//   const { campName } = data.name;
+//   const { campReviews } = data.rating;
+//   const { campPrice } = data.price;
+//   const { campUrl } = data.url;
+// }
+
+$(searchButton).on("click", function (event) {
+  event.preventDefault();
+  var searchResult = $("#city").val();
+  saveSearch(searchResult);
+  console.log(searchResult);
+});
